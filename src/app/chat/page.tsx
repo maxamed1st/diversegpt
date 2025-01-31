afaba1dca295f943f69ad77a67925940c6f49faf
maxamed1st <maxamed1st@gmail.com>
2025-01-31 21:25:42 +0100

    logout user

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,29 @@
+"use client";
+
+import { useSession, signOut, SessionProvider } from "next-auth/react"
+import { useRouter } from "next/navigation"
+
 export default function Chat() {
   return (
-    <div>
+    <SessionProvider>
+      <MainChatComponent />
+    </SessionProvider>
+  )
+}
+
+function MainChatComponent() {
+  const { data: session, status } = useSession();
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+  if (!session?.user) {
+    useRouter().push("/api/auth/signin");
+    return;
+  }
+  return (
+    <div className="flex flex-col h-screen text-center">
       <h1>Chat</h1>
+      <button onClick={() => signOut()}>Logout</button>
     </div>
   );
 }
