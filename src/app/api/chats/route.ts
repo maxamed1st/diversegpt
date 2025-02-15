270941f8ec4b4d54b882b6d3feb92a21234e3fb8
maxamed1st <maxamed1st@gmail.com>
2025-02-15 02:21:30 +0100

    rename folder to plural

--- src/app/api/chats/route.ts
+++ src/app/api/chats/route.ts
@@ -1,5 +1,5 @@
-import createChatHandler from "@/app/api/chat/handlers/createChat";
-import chatListHandler from "@/app/api/chat/handlers/chatList";
+import createChatHandler from "@/app/api/chats/handlers/createChat";
+import chatListHandler from "@/app/api/chats/handlers/chatList";
 
 export async function POST(request: Request) {
   return createChatHandler(request);
