67a1b0e55cd47dc003a67acf00f0e51dc7c9a228
maxamed1st <maxamed1st@gmail.com>
2025-03-14 21:18:05 +0100

    prepare for production and containarize app

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  eslint: {
+    // This will allow the build to complete even with ESLint errors
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
