6728aee99e8219cad555da91ee4ea1ee7f93a191
maxamed1st <maxamed1st@gmail.com>
2025-03-24 00:41:38 +0100

    decrease width on bigger screens

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -18,7 +18,7 @@ export default function Settings() {
   }
 
   return (
-    <div className="p-4 overflow-auto">
+    <div className="p-4 overflow-auto lg:px-16">
       <div className="space-y-6">
         <div>
           <h1 className="text-3xl font-bold mb-1">Settings</h1>
